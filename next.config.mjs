89a30bb8a529c05bddd209ed6a4a3353130a7d4d
luuizz <luu.izz@hotmail.com>
2024-06-08 17:24:24 -0300

    feat: configurado vanilla-extract

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+  const withVanillaExtract = createVanillaExtractPlugin();
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cdn.prismic.io',
+        },
+      ],
+    },
+  };
+  
+  export default withVanillaExtract(nextConfig);
