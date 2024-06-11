4b9ba38c6fe58d242c9bb1cd6198ad4fad76d15a
luuizz <luu.izz@hotmail.com>
2024-06-11 20:31:08 -0300

    listagem de posts

--- src/app/data.js
+++ src/app/data.js
@@ -1,6 +1,9 @@
 import iconTecnologia from "@/assets/icon-tecnologia.svg"
 import iconReact from "@/assets/icon-react.svg"
 import iconHtmlCss from "@/assets/icon-html-css.svg"
+import post05 from "@/assets/img-post-topic-05.jpg"
+import post04 from "@/assets/banner-react.jpg"
+import imageLG from "@/assets/img-feature.jpg"
 
 export const categorysLinks = [
     {
@@ -20,5 +23,35 @@ export const categorysLinks = [
         icon: iconHtmlCss,
         label: 'HTML & CSS',
         altText: 'Ícone de CSS',
+    },
+]
+
+export const postPopulars = [
+    {
+        category: 'HTML & CSS',
+        title: 'Como montar um código semântico com no HTML?',
+        date: '10, junho',
+        reading: '1 min de leitura',
+        image: imageLG,
+        path: '/post/teste',
+        alt: '',
+    },
+    {
+        category: 'ReactJS',
+        title: 'Trabalhando com Javascript e condições dentro do ReactJS',
+        date: '10, junho',
+        reading: '1 min de leitura',
+        image: post04,
+        path: '/post/teste',
+        alt: '',
+    },
+    {
+        category: 'Javascript',
+        title: 'Trabalhando com CSS em conjunto com ReactJS',
+        date: '10, junho',
+        reading: '1 min de leitura',
+        image: post05,
+        path: '/post/teste',
+        alt: '',
     },
 ]
