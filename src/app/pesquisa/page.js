253cbf39af221e9b02f7f987352c752c34c6cb26
luuizz <luu.izz@hotmail.com>
2024-06-11 20:31:27 -0300

    feat: inicio da query

--- src/app/pesquisa/page.js
+++ src/app/pesquisa/page.js
@@ -1,7 +1,13 @@
-import React from 'react'
+'use client'
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function PesquisaPage() {
+  const searchParams = useSearchParams()
+  const search = searchParams.get('s');
+
+
   return (
-    <div>PesquisaPage</div>
+    <h1>Resultados da Pesquisa para "{search}" </h1>
   )
 }
--- src/components/Header/Form/Form.js
+++ src/components/Header/Form/Form.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import RenderComputed from '../../RenderComputed'
+'use client'
 import iconSearch from '@/assets/icon-search.svg'
 import Image from 'next/image'
 import { styles } from './style.css'
+import { useRouter, useSearchParams } from 'next/navigation';
 
 export default function Form() {
+
+  const router = useRouter();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const searchQuery = event.target.s.value;
+    if (searchQuery) {
+      router.push(`/pesquisa?s=${searchQuery}`);
+    }
+  };
+
   return (
-    <RenderComputed asElement='form' asStyle={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
         <input className={styles.input} type="text" placeholder='Pesquise por artigo ou tema' name='s' />
         <button className={styles.button} type='submit'>
             <Image src={iconSearch} alt='Ícone de uma lupa' />
         </button>
-    </RenderComputed>
+    </form>
   )
 }
