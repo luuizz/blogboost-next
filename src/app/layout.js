174dbc0b4548219629e3849b4476013e8034b8d7
luuizz <luu.izz@hotmail.com>
2024-06-16 14:59:42 -0300

    fix: corrigido peso da font e adicionado footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,34 +1,35 @@
-import localFont from "next/font/local"
-import "./app.css"
+import localFont from "next/font/local";
+import "./app.css";
 import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer";
 
 const atypBLText = localFont({
   src: [
     {
-      path: './../fonts/AtypBLText-Regular.woff2',
-      weight: '500',
-      style: 'normal',
+      path: "./../fonts/AtypBLText-Regular.woff2",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: './../fonts/AtypBLText-Medium.woff2',
-      weight: '500',
-      style: 'normal',
+      path: "./../fonts/AtypBLText-Medium.woff2",
+      weight: "500",
+      style: "normal",
     },
     {
-      path: './../fonts/AtypBLText-Semibold.woff2',
-      weight: '600',
-      style: 'normal',
+      path: "./../fonts/AtypBLText-Semibold.woff2",
+      weight: "600",
+      style: "normal",
     },
     {
-      path: './../fonts/AtypBLText-Bold.woff2',
-      weight: '700',
-      style: 'normal',
+      path: "./../fonts/AtypBLText-Bold.woff2",
+      weight: "700",
+      style: "normal",
     },
   ],
-  display: 'swap',
-  variable: '--font-atyp',
-  adjustFontFallback: false
-})
+  display: "swap",
+  variable: "--font-atyp",
+  adjustFontFallback: false,
+});
 
 export const metadata = {
   title: "Codeboost Blog",
@@ -41,6 +42,7 @@ export default function RootLayout({ children }) {
       <body className={atypBLText.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
