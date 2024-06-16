fc4cd9fd3c413a389a273cc63f9a6fb83fda2b3b
luuizz <luu.izz@hotmail.com>
2024-06-16 15:00:04 -0300

    fix: adicionado novas variáveis

--- src/app/variables.css.js
+++ src/app/variables.css.js
@@ -1,22 +1,23 @@
-import { createGlobalTheme } from '@vanilla-extract/css';
+import { createGlobalTheme } from "@vanilla-extract/css";
 
-export const variable = createGlobalTheme(':root', {
-    pink: '#F67ACB',
-    purple: '#5B1FA6',
-    grayScale: {
-        100: '#050110',
-        200: '#0F0C1B',
-        300: '#3F3D49',
-        400: '#57555F',
-        500: '#ACB4BF',
-        alpha5: '153, 149, 166',
-    },
-    darkScale: '#1C162E',
-    white: '#fff',
-    black: '#000',
-    headerHeight: '8rem',
-    lineHeight: {
-      short: '120%',
-      base: '150%',
-    },
-  });
+export const variable = createGlobalTheme(":root", {
+  pink: "#F67ACB",
+  purple: "#5B1FA6",
+  grayScale: {
+    100: "#050110",
+    200: "#0F0C1B",
+    300: "#3F3D49",
+    400: "#57555F",
+    500: "#9995A6",
+    600: "#ACB4BF",
+    alpha5: "153, 149, 166",
+  },
+  darkScale: "#1C162E",
+  white: "#fff",
+  black: "#000",
+  headerHeight: "8rem",
+  lineHeight: {
+    short: "120%",
+    base: "150%",
+  },
+});
